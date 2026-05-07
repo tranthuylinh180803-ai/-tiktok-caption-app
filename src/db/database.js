5c3a6751df45ef7e2b5f3ff10bddb64669f04c52
Tran Thuy Linh <tranthuylinh180803@gmail.com>
2026-05-07 09:27:27 -0700

    debug: add detailed DB error logging, test internal URL with SSL

--- src/db/database.js
+++ src/db/database.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
-  connectionTimeoutMillis: 15000,
+  connectionTimeoutMillis: 20000,
   idleTimeoutMillis: 30000
 });
 
